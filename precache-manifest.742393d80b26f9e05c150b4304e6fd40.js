47f40bfe74dc771326308012d22fa38dbea6455f
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-09-03 14:33:42 -0700

    Updates

---
+++ precache-manifest.742393d80b26f9e05c150b4304e6fd40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdab9b3473b6ab6489ca7aeaf8c9cfa",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "d251a29df2bdc2438cbd",
+    "url": "/shoppies/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "9cf8728f7ca79caaf946",
+    "url": "/shoppies/static/css/main.2667bfb9.chunk.css"
+  },
+  {
+    "revision": "d251a29df2bdc2438cbd",
+    "url": "/shoppies/static/js/2.bce3b25e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppies/static/js/2.bce3b25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf8728f7ca79caaf946",
+    "url": "/shoppies/static/js/main.18335e12.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  }
+]);
